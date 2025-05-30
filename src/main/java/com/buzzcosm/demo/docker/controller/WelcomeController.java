f400fb1d4454cdcdd940fdd5451fa3c0b84bff88
Evgueni Kounik <evgueni.kounik@aclevion.com>
2025-05-30 13:56:23 +0200

    init commit

---
+++ src/main/java/com/buzzcosm/demo/docker/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.buzzcosm.demo.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class WelcomeController {
+
+    @GetMapping
+    public String welcome() {
+        return "Hello API 👋";
+    }
+}
